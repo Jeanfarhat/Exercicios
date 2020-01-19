fd65378e7fd9789a71a33fa1da3ad29927b9412f
jeanfarhat <jeanfarhat7@gmail.com>
2020-01-19 19:23:29 -0300

    Exercícios resolvidos do 1001 ao 1005

---
+++ URI_Python_Beginner.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 19:09:40 2020
+
+@author: Jean
+"""
+#Exercícios Python Beginner
+
+#1001
+
+a= int(input())
+b= int(input())
+x=(a+b)
+(print("X =", x))
+
+#1002
+
+n=3.14159
+R= float(input())
+a=n*R*R
+print("A=%.4f"% a)
+
+#1003
+
+A = int(input())
+B = int(input())
+SOMA = (A+B)
+
+print("SOMA =", SOMA)
+
+#1004
+
+a = int(input())
+b = int(input())
+PROD = a*b
+print("PROD =", PROD)
